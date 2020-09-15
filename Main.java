95f061b9d79e0bb8d7121f742127634a2bf9ef91
morrc3611 <morrc3611@wrdsb.ca>
2020-09-15 12:19:56 +0000

    Checks if number is even or odd

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 /**
- *
- * @author 
+ * This program checks if an integer is even or odd
+ * @chad
  */
 public class Main {
 
@@ -9,7 +11,24 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // create a Scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    final int DIVISOR = 2;
+    final int EVEN = 0;
+    //tells user to input a number and records that number
+    System.out.println("Please enter an integer:");
+    int number = input.nextInt();
+     
+   //divides integer by 2 
+   int checkNum = number % DIVISOR;
+   
+   //checks to see if the remainder is 1 or 0
+   if (checkNum == EVEN){
+     System.out.println(number + " is an even number");
+   } else {
+     System.out.println(number + " is an odd number");
+   }
+
   }
 }
